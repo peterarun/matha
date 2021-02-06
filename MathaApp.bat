5c1806c036d0406d01ad28efd9ee20dd67777af1
Arun Peter <arunpeter@gmail.com>
2021-02-06 18:04:18 +0530

    Added Chennai properties and batch

--- MathaApp.bat
+++ MathaApp.bat
@@ -1,1 +1,1 @@
-start "" javaw -jar sales-0.0.1-SNAPSHOT.jar 2>&1 > javalog.log 
+java -jar -Dspring.profiles.active=vazhakulam -jar sales-0.0.1-SNAPSHOT.jar 2>&1 > javalog.log
---
+++ MathaDistApp.bat
@@ -0,0 +1,1 @@
+java -jar -Dspring.profiles.active=chennai -jar sales-0.0.1-SNAPSHOT.jar 2>&1 > javalog.log
